93f070f265f056d7290f2197440eaa9df4ddfe76
forverunu13 <dzhondayviharris994@gmail.com>
2025-12-13 18:26:49 +0700

    start todo list

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/about")
+def about():
+    return {"author": "Сергей Рогожкин"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": "BLUBLUBLU"}
+
+
+@app.post("/tasks")
+def post_tasks():
+    return {"ULBULBULB"}
+
+
+@app.delete("/tasks/{id}")
+def delete_tasks():
+    return {"..."}
